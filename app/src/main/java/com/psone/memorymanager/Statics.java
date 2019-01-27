a94135f5cc650cee7ee14501212f42d2ed63c638
Topher Lee <topher.lee.13@gmail.com>
2019-01-27 14:26:11 -0500

    Not exactly sure, cleaning up though...

---
+++ app/src/main/java/com/psone/memorymanager/Statics.java
@@ -0,0 +1,22 @@
+package com.psone.memorymanager;
+
+import com.topher.psxmc.R;
+
+
+public class Statics {
+	
+	static MemoryCard[] cards = new MemoryCard[]{null,null};
+	static boolean changed = false;
+	static boolean backup = true;
+	static boolean export = true;
+	static int exportFmt = 0;
+	static String defaultPath = null;
+	
+	static byte[] copyArray(byte[] a, int size){
+		byte[] b = new byte[size];
+		for(int i = 0;i<a.length;i++){
+			b[i] = a[i];
+		}
+		return b;
+	}
+}
